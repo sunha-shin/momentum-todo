e185d19c5ad215bbcb4891e50afbc61ad8f69251
Summer Shin <shinsunha11@gmail.com>
2023-05-29 17:59:50 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // @ts-nocheck
-import styled from 'styled-components';
-import {useRoutes} from 'react-router-dom';
+import styled from "styled-components";
+import { useRoutes } from "react-router-dom";
 import Main from "./views/pages/Main";
 import Info from "./views/pages/Info";
 
 export default function App() {
-    const infoComp = {path: '/', element: <Info/>};
+  const infoComp = { path: "/", element: <Info /> };
 
-    let element = useRoutes([
-        infoComp,
-        {path: '/main', element: <Main/>},
-    ]);
+  let element = useRoutes([infoComp, { path: "/main", element: <Main /> }]);
 
-    return <Container>{element}</Container>;
+  console.log("test");
+
+  return <Container>{element}</Container>;
 }
 
 const Container = styled.div`
-  background: url('https://source.unsplash.com/daily') no-repeat center center fixed;
+  background: url("https://source.unsplash.com/daily") no-repeat center center
+    fixed;
   background-size: cover;
 `;
